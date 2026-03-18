aa5c51b1abcb07ac997f1e79cd183606b5a2edb7
ankitrout07 <ankitanupam3@gmail.com>
2026-03-18 18:53:13 +0530

    update vms

--- main.tf
+++ main.tf
@@ -104,10 +104,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
     storage_account_type = "Standard_LRS"
   }
 
-  source_image_reference {
+# Update this block in your main.tf
+source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    # Ensure the SKU specifies 'arm64'
+    sku       = "22_04-lts-arm64" 
     version   = "latest"
   }
 
--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-  default = "centralindia" 
+  default = "centralindia"
 }
 
 variable "vm_size" {
